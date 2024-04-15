5e7bf5f3cd1974fb038488e4ca40686027c2111f
Filippo Viti <filippo.viti2002@gmail.com>
2024-04-15 10:39:52 +0200

    Updated rail IDs

--- convert.py
+++ convert.py
@@ -2,14 +2,14 @@ import base64
 import zlib
 import pyperclip
 
-blueprint_string = input('Insert blueprint string:\n')
+blueprint_string = input('Paste your blueprint string:\n')
 blueprint_string = blueprint_string[1:]
 blueprint_string = base64.b64decode(blueprint_string)
 blueprint_string = zlib.decompress(blueprint_string)
 blueprint_string = blueprint_string.decode('utf-8')
 
-converted_blueprint = (blueprint_string.replace('straight-rail', 'straight-space-rail')
-                       .replace('curved-rail', 'curved-space-rail'))
+converted_blueprint = (blueprint_string.replace('straight-rail', 'se-space-straight-rail')
+                       .replace('curved-rail', 'se-space-curved-rail'))
 converted_blueprint = converted_blueprint.encode('utf-8')
 converted_blueprint = zlib.compress(converted_blueprint, 9)
 converted_blueprint = '0' + base64.b64encode(converted_blueprint).decode('utf-8')
