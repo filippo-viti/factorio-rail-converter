327ae3c9e916bd7f18da8d9da7ad635f68e65bec
Filippo Viti <filippo.viti2002@gmail.com>
2024-04-15 10:28:47 +0200

    Converted blueprint is now copied to clipboard

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
 import base64
 import zlib
+import pyperclip
 
-blueprint_string = input('Insert blueprint string:')
+blueprint_string = input('Insert blueprint string:\n')
 blueprint_string = blueprint_string[1:]
 blueprint_string = base64.b64decode(blueprint_string)
 blueprint_string = zlib.decompress(blueprint_string)
@@ -13,4 +14,5 @@ converted_blueprint = converted_blueprint.encode('utf-8')
 converted_blueprint = zlib.compress(converted_blueprint, 9)
 converted_blueprint = '0' + base64.b64encode(converted_blueprint).decode('utf-8')
 
-print('Converted blueprint:\n' + converted_blueprint)
+pyperclip.copy(converted_blueprint)
+print('Your converted blueprint has been copied to clipboard!')
