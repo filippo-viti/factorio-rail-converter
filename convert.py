7d5980165563c13da2c8f4d08040503d0b1b71cf
Filippo Viti <filippo.viti2002@gmail.com>
2024-04-15 10:15:26 +0200

    Added conversion

--- convert.py
+++ convert.py
@@ -4,8 +4,13 @@ import zlib
 blueprint_string = input('Insert blueprint string:')
 blueprint_string = blueprint_string[1:]
 blueprint_string = base64.b64decode(blueprint_string)
-
 blueprint_string = zlib.decompress(blueprint_string)
 blueprint_string = blueprint_string.decode('utf-8')
 
-print('Converted blueprint:\n' + blueprint_string)
+converted_blueprint = (blueprint_string.replace('straight-rail', 'straight-space-rail')
+                       .replace('curved-rail', 'curved-space-rail'))
+converted_blueprint = converted_blueprint.encode('utf-8')
+converted_blueprint = zlib.compress(converted_blueprint, 9)
+converted_blueprint = '0' + base64.b64encode(converted_blueprint).decode('utf-8')
+
+print('Converted blueprint:\n' + converted_blueprint)
